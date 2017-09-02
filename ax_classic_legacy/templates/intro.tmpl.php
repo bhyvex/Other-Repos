88d1de2c18e4751262bcf74b65e3c2dfec0061ed
patrikpatrik <axclassiceq@gmail.com>
2017-09-01 23:33:45 -0700

    Updated

--- ax_classic_legacy/templates/intro.tmpl.php
+++ ax_classic_legacy/templates/intro.tmpl.php
@@ -1,5 +1,5 @@
        <center>
         <div class="page_title"><span><br>AX-Classic Online Editing Interface</span></div>
         <img src="images/dontpanic.jpg">
-        <div class="page_title"><span>Boomba The Big Revised: June 11, 2017</span></div>
+        <div class="page_title"><span>Boomba The Big Revised: September 1, 2017</span></div>
        </center>
