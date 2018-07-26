4ee77014dd7b0237e6936ae1d535c16d109c98a7
patrikpatrik <axclassiceq@gmail.com>
2018-07-26 09:32:50 -0700

    addition of chat feature for php editor v2.0

--- ax_classic_legacy/templates/intro.tmpl.php
+++ ax_classic_legacy/templates/intro.tmpl.php
@@ -1,11 +1,11 @@
        <center>
         <div class="page_title"><span><br>AX-Classic Online Editing Interface</span></div>
         <img src="images/dontpanic.jpg" style="border: 3px inset #879caf;">
-        <div class="page_title"><span>Boomba The Big Revised: April 18, 2018</span></div>
+        <div class="page_title"><span>Boomba The Big Revised: July 28, 2018</span></div>
        <p class="editor">
 	   <?php
 			error_reporting(E_ALL); //(E_ALL, 0)
-			$editor = "http://www.axclassic.com/changelog_php.txt";
+			$editor = "https://www.axclassic.com/changelog_php.txt";
 			$myfile = fopen($editor, "rb");
 			
 			for($i=0;$i<18;$i++) {
